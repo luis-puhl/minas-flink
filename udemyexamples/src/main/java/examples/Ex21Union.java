8a491a6b1332bad64daa0fb5d8ab69eab424c6b5
Luis Puhl <luispuhl@gmail.com>
2019-11-19 18:32:15 -0300

    Formating

--- udemyexamples/src/main/java/examples/Ex21Union.java
+++ udemyexamples/src/main/java/examples/Ex21Union.java
@@ -1,24 +1,12 @@
 package examples;
 
-import org.apache.flink.api.common.functions.RichFlatMapFunction;
-import org.apache.flink.api.common.state.ReducingState;
-import org.apache.flink.api.common.state.ReducingStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
-import org.apache.flink.util.Collector;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 public class Ex21Union {
     public static void main(String[] args) throws Exception {
@@ -26,16 +14,17 @@ public class Ex21Union {
         final ParameterTool params = ParameterTool.fromArgs(args);
         env.getConfig().setGlobalJobParameters(params);
 
-        TypeInformation<Tuple3<String, String, Double>> info = TypeInformation.of(new TypeHint<Tuple3<String, String, Double>>(){});
+        TypeInformation<Tuple3<String, String, Double>> info = TypeInformation.of(new TypeHint<Tuple3<String, String, Double>>() {
+        });
         DataStream<Tuple3<String, String, Double>> source = StreamUtil.getDataStream(env, params)
-            .map((String src) -> {
-                if (src == null || src.length() < 5) {
-                    src = "(BRL,35.74316815422196,37.15008714497207)";
-                }
-                String[] srcList = src.trim().replaceAll("[\\(\\)]", "").split(",");
-                Double openValue = Double.parseDouble(srcList[1]);
-                return new Tuple3<>(srcList[0], srcList[1] + " " + srcList[2], openValue);
-            }).returns(info);
+                .map((String src) -> {
+                    if (src == null || src.length() < 5) {
+                        src = "(BRL,35.74316815422196,37.15008714497207)";
+                    }
+                    String[] srcList = src.trim().replaceAll("[\\(\\)]", "").split(",");
+                    Double openValue = Double.parseDouble(srcList[1]);
+                    return new Tuple3<>(srcList[0], srcList[1] + " " + srcList[2], openValue);
+                }).returns(info);
 
         // sum numbers
         DataStream<Tuple3<String, String, Double>> stream1 = source
