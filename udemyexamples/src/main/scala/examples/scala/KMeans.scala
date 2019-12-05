785bda366b2a17d7a5e92140e15d0df288f03e2b
Luis Puhl <luispuhl@gmail.com>
2019-12-05 18:41:40 -0300

    translating Kmeans Data and Data Generator to scala

--- udemyexamples/src/main/scala/examples/scala/KMeans.scala
+++ udemyexamples/src/main/scala/examples/scala/KMeans.scala
@@ -17,6 +17,10 @@
  */
 package examples.scala
 
+import java.io.{BufferedWriter, File, FileWriter, IOException}
+import java.text.DecimalFormat
+
+import scala.collection.mutable.StringBuilder
 import org.apache.flink.api.common.functions._
 import org.apache.flink.api.java.functions.FunctionAnnotation.ForwardedFields
 import org.apache.flink.api.java.utils.ParameterTool
@@ -24,6 +28,7 @@ import org.apache.flink.api.scala._
 import org.apache.flink.configuration.Configuration
 
 import scala.collection.JavaConverters._
+import scala.util.{Random, Try}
 
 /**
  * This example implements a basic K-Means clustering algorithm.
@@ -47,7 +52,7 @@ import scala.collection.JavaConverters._
  *
  * Input files are plain text files and must be formatted as follows:
  *
- * - Data points are represented as two double values separated by a blank character.
+ * - Data points are represented as two Double values separated by a blank character.
  * Data points are separated by newline characters.
  * For example `"1.2 2.3\n5.3 7.2\n"` gives two data points (x=1.2, y=2.3) and (x=5.3,
  * y=7.2).
@@ -60,7 +65,7 @@ import scala.collection.JavaConverters._
  *   KMeans --points <path> --centroids <path> --output <path> --iterations <n>
  * }}}
  * If no parameters are provided, the program is run with default data from
- * [[org.apache.flink.examples.java.clustering.util.KMeansData]]
+ * KMeansData
  * and 10 iterations.
  *
  * This example shows how to use:
@@ -72,17 +77,15 @@ import scala.collection.JavaConverters._
 object KMeans {
 
   def main(args: Array[String]) {
-
     // checking input parameters
     val params: ParameterTool = ParameterTool.fromArgs(args)
-
     // set up execution environment
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
 
     // get input data:
     // read the points and centroids from the provided paths or fall back to default data
-    val points: DataSet[Point] = getPointDataSet(params, env)
-    val centroids: DataSet[Centroid] = getCentroidDataSet(params, env)
+    val points: DataSet[Point] = KMeansData.getPointDataSet(params, env)
+    val centroids: DataSet[Centroid] = KMeansData.getCentroidDataSet(params, env)
 
     val finalCentroids = centroids.iterate(params.getInt("iterations", 10)) { currentCentroids =>
       val newCentroids = points
@@ -104,17 +107,12 @@ object KMeans {
       println("Printing result to stdout. Use --output to specify output path.")
       clusteredPoints.print()
     }
-
   }
 
-  // *************************************************************************
-  //     DATA TYPES
-  // *************************************************************************
-
   /**
-    * Common trait for operations supported by both points and centroids
-    * Note: case class inheritance is not allowed in Scala
-    */
+   * Common trait for operations supported by both points and centroids
+   * Note: case class inheritance is not allowed in Scala
+   */
   trait Coordinate extends Serializable {
 
     var x: Double
@@ -146,22 +144,19 @@ object KMeans {
   }
 
   /**
-    * A simple two-dimensional point.
-    */
+   * A simple two-dimensional point.
+   */
   case class Point(var x: Double = 0, var y: Double = 0) extends Coordinate
 
   /**
-    * A simple two-dimensional centroid, basically a point with an ID.
-    */
+   * A simple two-dimensional centroid, basically a point with an ID.
+   */
   case class Centroid(var id: Int = 0, var x: Double = 0, var y: Double = 0) extends Coordinate {
-
     def this(id: Int, p: Point) {
       this(id, p.x, p.y)
     }
 
-    override def toString: String =
-      s"$id ${super.toString}"
-
+    override def toString: String = s"$id ${super.toString}"
   }
 
   /** Determines the closest cluster center for a data point. */
@@ -188,44 +183,43 @@ object KMeans {
     }
 
   }
+
   object KMeansData {
     def getCentroidDataSet(params: ParameterTool, env: ExecutionEnvironment): DataSet[Centroid] =
       env.fromCollection(List(
-        Centroid(1, -31.85, -44.77),  Centroid(2, 35.16, 17.46),
-        Centroid(3, -5.16, 21.93),    Centroid(4, -24.06, 6.81),
+        Centroid(1, -31.85, -44.77), Centroid(2, 35.16, 17.46),
+        Centroid(3, -5.16, 21.93), Centroid(4, -24.06, 6.81),
       ))
 
     def getPointDataSet(params: ParameterTool, env: ExecutionEnvironment): DataSet[Point] = {
       val points = List(
-        (-14.22, -48.01), (-22.78, 37.10),    (56.18, -42.99),    (35.04, 50.29),
-        (-9.53, -46.26),  (-34.35, 48.25),    (55.82, -57.49),    (21.03, 54.64),
-        (-13.63, -42.26), (-36.57, 32.63),    (50.65, -52.40),    (24.48, 34.04),
-        (-2.69, -36.02),  (-38.80, 36.58),    (24.00, -53.74),    (32.41, 24.96),
-        (-4.32, -56.92),  (-22.68, 29.42),    (59.02, -39.56),    (24.47, 45.07),
-        (5.23, -41.20),   (-23.00, 38.15),    (44.55, -51.50),    (14.62, 59.06),
-        (7.41, -56.05),   (-26.63, 28.97),    (47.37, -44.72),    (29.07, 51.06),
-        (0.59, -31.89),   (-39.09, 20.78),    (42.97, -48.98),    (34.36, 49.08),
-        (-21.91, -49.01), (-46.68, 46.04),    (48.52, -43.67),    (30.05, 49.25),
-        (4.03, -43.56),   (-37.85, 41.72),    (38.24, -48.32),    (20.83, 57.85)
+        (-14.22, -48.01), (-22.78, 37.10), (56.18, -42.99), (35.04, 50.29),
+        (-9.53, -46.26), (-34.35, 48.25), (55.82, -57.49), (21.03, 54.64),
+        (-13.63, -42.26), (-36.57, 32.63), (50.65, -52.40), (24.48, 34.04),
+        (-2.69, -36.02), (-38.80, 36.58), (24.00, -53.74), (32.41, 24.96),
+        (-4.32, -56.92), (-22.68, 29.42), (59.02, -39.56), (24.47, 45.07),
+        (5.23, -41.20), (-23.00, 38.15), (44.55, -51.50), (14.62, 59.06),
+        (7.41, -56.05), (-26.63, 28.97), (47.37, -44.72), (29.07, 51.06),
+        (0.59, -31.89), (-39.09, 20.78), (42.97, -48.98), (34.36, 49.08),
+        (-21.91, -49.01), (-46.68, 46.04), (48.52, -43.67), (30.05, 49.25),
+        (4.03, -43.56), (-37.85, 41.72), (38.24, -48.32), (20.83, 57.85)
       )
-      val pointsCollection: List[Point] = points.map[Point](x => Point(x._1, x._2))
+      val pointsCollection: List[Point] = points.map(x => Point(x._1, x._2))
       env.fromCollection(pointsCollection)
     }
   }
-}
 
-/*
-object KMeansDataGenerator {
+  object KMeansDataGenerator {
     val CENTERS_FILE: String = "centers"
     val POINTS_FILE: String = "points"
-    val DEFAULT_SEED: long = 4650285087650871364L
-    val DEFAULT_VALUE_RANGE: double = 100.0
-    val RELATIVE_STDDEV: double = 0.08
-    val DIMENSIONALITY: int = 2
+    val DEFAULT_SEED: Long = 4650285087650871364L
+    val DEFAULT_VALUE_RANGE: Double = 100.0
+    val RELATIVE_STDDEV: Double = 0.08
+    val DIMENSIONALITY: Int = 2
     val FORMAT: DecimalFormat = new DecimalFormat("#0.00")
-    val DELIMITER: char = ' '
+    val DELIMITER: Char = ' '
 
-    // /**
+    /**
      * Main method to generate data for the {@link KMeans} example program.
      *
      * <p>The generator creates to files:
@@ -243,124 +237,77 @@ object KMeansDataGenerator {
      * <li><b>Optional</b> Double: Value range of cluster centers
      * <li><b>Optional</b> Long: Random seed
      * </ol>
-     *
      * @throws IOException
      */
-    public static void main(String[] args) throws IOException {
+    def main(args: Array[String]) = {
+      // check parameter count
+      if (args.length < 2)
+        throw new RuntimeException("KMeansDataGenerator -points <num> -k <num clusters> [-output <output-path>] [-stddev <relative stddev>] [-range <centroid range>] [-seed <seed>]")
+      // parse parameters
+      val params: ParameterTool = ParameterTool.fromArgs(args)
+      // set up execution environment
+      val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
 
-        // check parameter count
-        if (args.length < 2) {
-            System.out.println("KMeansDataGenerator -points <num> -k <num clusters> [-output <output-path>] [-stddev <relative stddev>] [-range <centroid range>] [-seed <seed>]");
-            System.exit(1);
-        }
+      val numDataPoints: Int = params.getInt("points")
+      val k: Int = params.getInt("k")
+      val outDir: String = params.get("output", System.getProperty("java.io.tmpdir"))
+      val stddev: Double = params.getDouble("stddev", RELATIVE_STDDEV)
+      val range: Double = params.getDouble("range", DEFAULT_VALUE_RANGE)
+      val firstSeed: Long = params.getLong("seed", DEFAULT_SEED)
 
-        // parse parameters
+      val absoluteStdDev: Double = stddev * range
+      val random: Random = new Random(firstSeed)
 
-        final ParameterTool params = ParameterTool.fromArgs(args);
-        final int numDataPoints = params.getInt("points");
-        final int k = params.getInt("k");
-        final String outDir = params.get("output", System.getProperty("java.io.tmpdir"));
-        final double stddev = params.getDouble("stddev", RELATIVE_STDDEV);
-        final double range = params.getDouble("range", DEFAULT_VALUE_RANGE);
-        final long firstSeed = params.getLong("seed", DEFAULT_SEED);
+      // the means around which data points are distributed
+      val means = uniformRandomCenters(random, k, DIMENSIONALITY, range);
 
-        final double absoluteStdDev = stddev * range;
-        final Random random = new Random(firstSeed);
+      // write the points out
+      val buffer: StringBuilder = new StringBuilder()
+      (for {
+        pointsOut <- Try { new BufferedWriter(new FileWriter(new File(outDir + "/" + POINTS_FILE))) }.toOption.toList
+        point <- (0 to numDataPoints)
+          .map(i => means(i % means.size)
+            .map(ci => ci + (random.nextGaussian() * absoluteStdDev)))
+        w <- writePoint(point, buffer, pointsOut)
+      } yield pointsOut).andThen(p => p.close())
 
-        // the means around which data points are distributed
-        final double[][] means = uniformRandomCenters(random, k, DIMENSIONALITY, range);
+      // write the uniformly distributed centers to a file
+      (for {
+        centersOut: BufferedWriter <- Try { new BufferedWriter(new FileWriter(new File(outDir + "/" + CENTERS_FILE))) }.toOption.toList
+        center <- (1 to k)
+          .map(i => (i,
+            (0 to DIMENSIONALITY).map(j => (random.nextDouble() * range) - (range / 2))
+          )).toList
+        w <- writeCenter(center._1, center._2, buffer, centersOut)
+      } yield centersOut).andThen(p => p.close())
 
-        // write the points out
-        BufferedWriter pointsOut = null;
-        try {
-            pointsOut = new BufferedWriter(new FileWriter(new File(outDir + "/" + POINTS_FILE)));
-            StringBuilder buffer = new StringBuilder();
-
-            double[] point = new double[DIMENSIONALITY];
-            int nextCentroid = 0;
-
-            for (int i = 1; i <= numDataPoints; i++) {
-                // generate a point for the current centroid
-                double[] centroid = means[nextCentroid];
-                for (int d = 0; d < DIMENSIONALITY; d++) {
-                    point[d] = (random.nextGaussian() * absoluteStdDev) + centroid[d];
-                }
-                writePoint(point, buffer, pointsOut);
-                nextCentroid = (nextCentroid + 1) % k;
-            }
-        }
-        finally {
-            if (pointsOut != null) {
-                pointsOut.close();
-            }
-        }
-
-        // write the uniformly distributed centers to a file
-        BufferedWriter centersOut = null;
-        try {
-            centersOut = new BufferedWriter(new FileWriter(new File(outDir + "/" + CENTERS_FILE)));
-            StringBuilder buffer = new StringBuilder();
-
-            double[][] centers = uniformRandomCenters(random, k, DIMENSIONALITY, range);
-
-            for (int i = 0; i < k; i++) {
-                writeCenter(i + 1, centers[i], buffer, centersOut);
-            }
-        }
-        finally {
-            if (centersOut != null) {
-                centersOut.close();
-            }
-        }
-
-        System.out.println("Wrote " + numDataPoints + " data points to " + outDir + "/" + POINTS_FILE);
-        System.out.println("Wrote " + k + " cluster centers to " + outDir + "/" + CENTERS_FILE);
+      println("Wrote " + numDataPoints + " data points to " + outDir + "/" + POINTS_FILE);
+      println("Wrote " + k + " cluster centers to " + outDir + "/" + CENTERS_FILE);
     }
 
-    private static double[][] uniformRandomCenters(Random rnd, int num, int dimensionality, double range) {
-        final double halfRange = range / 2;
-        final double[][] points = new double[num][dimensionality];
+    def uniformRandomCenters(rnd: Random, num: Int, dimensionality: Int, range: Double): List[List[Double]] =
+      List.fill(num)(List.fill(dimensionality)(() => (rnd.nextDouble() * range) - (range / 2)).map(_.apply()))
 
-        for (int i = 0; i < num; i++) {
-            for (int dim = 0; dim < dimensionality; dim++) {
-                points[i][dim] = (rnd.nextDouble() * range) - halfRange;
-            }
-        }
-        return points;
+    def writePoint(coordinates: Seq[Double], buffer: StringBuilder, out: BufferedWriter): Seq[Double] = {
+      buffer.setLength(0)
+      coordinates.map[String](FORMAT.format).addString(buffer, DELIMITER.toString)
+      out.write(buffer.toString())
+      out.newLine()
+      coordinates
     }
 
-    private static void writePoint(double[] coordinates, StringBuilder buffer, BufferedWriter out) throws IOException {
-        buffer.setLength(0);
+    def writeCenter(id: Long, coordinates: Seq[Double], buffer: StringBuilder, out: BufferedWriter): Seq[Double] = {
+      buffer.setLength(0)
+      // write id
+      buffer.append(id)
+      buffer.append(DELIMITER)
 
-        // write coordinates
-        for (int j = 0; j < coordinates.length; j++) {
-            buffer.append(FORMAT.format(coordinates[j]));
-            if (j < coordinates.length - 1) {
-                buffer.append(DELIMITER);
-            }
-        }
-
-        out.write(buffer.toString());
-        out.newLine();
+      coordinates.map[String](FORMAT.format).addString(buffer, DELIMITER.toString)
+      out.write(buffer.toString())
+      out.newLine()
+      coordinates
     }
+  }
 
-    private static void writeCenter(long id, double[] coordinates, StringBuilder buffer, BufferedWriter out) throws IOException {
-        buffer.setLength(0);
-
-        // write id
-        buffer.append(id);
-        buffer.append(DELIMITER);
-
-        // write coordinates
-        for (int j = 0; j < coordinates.length; j++) {
-            buffer.append(FORMAT.format(coordinates[j]));
-            if (j < coordinates.length - 1) {
-                buffer.append(DELIMITER);
-            }
-        }
-
-        out.write(buffer.toString());
-        out.newLine();
-    }
 }
-*/
+
