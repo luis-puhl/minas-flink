af22f447c25d1ecf3dc4c5ca1268f841e60b3ac8
Luis Puhl <luispuhl@gmail.com>
2020-04-20 13:06:26 -0300

    adding java and maven m-fog variant

---
+++ mfog-java/src/main/java/br/ufscar/dc/ppgcc/gsdr/mfog/ModelStore.java
@@ -0,0 +1,8 @@
+package br.ufscar.dc.ppgcc.gsdr.mfog;
+
+public class ModelStore {
+    public static void main(String[] args) {
+        System.out.println("help");
+        StreamExecutionEnviroment
+    }
+}
