aa4bd800fd17d6390bb3fd5837a5d175535d4995
Luis Puhl <luispuhl@gmail.com>
2020-06-24 18:25:28 -0300

    Seedup calculations...
    local 4 core 45.97% time reduction
    remote 4 core 38.90%
    cluster 8 core 40.98%

--- mfog-mpi/loadenv.h
+++ mfog-mpi/loadenv.h
@@ -7,10 +7,10 @@ int assingVarFromEnvArg(char *varName, char **varPtr, char *envOrArg, char *next
 int loadEnv(int argc, char *argv[], char **envp, int varsSize, char *varNames[], char **varPtrs[], FILE **filePtrs[], char *fileModes[]);
 void closeEnv(int varsSize, char *varNames[], char **varPtrs[], FILE **filePtrs[], char *fileModes[]);
 
-// # source, executable, build_date-time, wall-clock, function, elapsed, cores, itens
-#define PRINT_TIMING_FORMAT "%s,%s,%s %s,%ld,%s,%e,%d,%d\n"
+// # source, executable, build_date-time, wall-clock, function, gnuc, elapsed, cores, itens
+#define PRINT_TIMING_FORMAT "%s,%s,%s %s,%ld,%s,%s,%e,%d,%d\n"
 #define PRINT_TIMING_ARGUMENTS(executable, nProcesses, start, nItens) \
-    __FILE__, executable, __DATE__, __TIME__, time(NULL), __FUNCTION__, ((double)(clock() - start)) / 1000000.0, nProcesses, nItens
+    __FILE__, executable, __DATE__, __TIME__, time(NULL), __FUNCTION__, __VERSION__, ((double)(clock() - start)) / 1000000.0, nProcesses, nItens
 #define PRINT_TIMING(timing, executable, nProcesses, start, nItens)                                      \
     fprintf(timing, PRINT_TIMING_FORMAT, PRINT_TIMING_ARGUMENTS(executable, nProcesses, start, nItens)); \
     fprintf(stderr, PRINT_TIMING_FORMAT, PRINT_TIMING_ARGUMENTS(executable, nProcesses, start, nItens))
