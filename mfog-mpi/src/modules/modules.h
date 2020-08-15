e7ede883b73a270f2eff21a83399388248d4c2a8
Luis Puhl <luispuhl@gmail.com>
2020-08-14 21:27:30 -0300

    Mfog split modules

--- mfog-mpi/src/modules/modules.h
+++ mfog-mpi/src/modules/modules.h
@@ -1,4 +1,10 @@
 
+#include "../baseline/base.h"
+#include "../util/net.h"
+
 #define MODEL_STORE_PORT 7000
 // #define MODEL_STORE_REQUEST_GET_ALL "get complete model\n"
 // #define MODEL_STORE_REQUEST_SEND_ALL "# Model(dimension="
+
+int appendClusterFromStore(Params *params, SOCKET modelStore, char *buffer, size_t buffSize, Model *model);
+int modelStoreComm(Params *params, int timeout, Model *model, SOCKET modelStore, struct pollfd *modelStorePoll, char *buffer, size_t maxBuffSize);
