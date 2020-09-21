a75ea3504ca1a3a94832dd7046f0ae516209cba9
Luis Puhl <luispuhl@gmail.com>
2020-09-21 11:36:32 -0300

    [mfog] WIP Redis Model Store at aws ec2.

---
+++ src/modules/redis/send-model.py
@@ -0,0 +1,21 @@
+import redis
+import sys
+import csv
+
+if __name__ == "__main__":
+    # main(args)
+    cli = redis.Redis.from_url('redis://ec2-18-191-2-174.us-east-2.compute.amazonaws.com:6379')
+    print('ping', cli.ping())
+    broker = cli.pubsub()
+
+    data = sys.stdin.readlines()
+    for line in csv.reader(data):
+    # for line in data:
+        if line[0].startswith('#'):
+            continue
+        id = int(line[0])
+        fullline = ','.join(line)
+        print(fullline, line)
+        cli.publish('model', fullline)
+
+    cli.close()
